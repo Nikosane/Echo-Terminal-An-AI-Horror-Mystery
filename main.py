24ccfdf3a1cbb0f9982d140bc40ff8c42c34ecd2
Nikosane <linux.nikosane@gmail.com>
2025-04-08 23:50:52 +0530

    entry point

---
+++ main.py
@@ -0,0 +1,9 @@
+from game_engine.game_loop import start_game
+
+def main():
+    print("\033[95m" + "\n[ ECHO TERMINAL ]" + "\033[0m")
+    print("You awaken to the hum of circuitry. The void greets you...")
+    start_game()
+
+if __name__ == "__main__":
+    main()
